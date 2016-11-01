12022ae6e23336e5f28895e5a24301a33f054bca
Paul Maserrat <pmaserrat@gmail.com>
2016-10-31 23:49:32 -0400

    Initial commit

---
+++ WebContent/index.jsp
@@ -0,0 +1,61 @@
+<html>
+	<head>
+		<title>Login</title>
+	</head>
+		<link rel="stylesheet" href="css/global.css"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" /></script>
+		<style type="text/css">
+			body {
+				background-color:#e6e6e6!important;
+			}
+			
+			.col-centered{
+			    float: none!important;
+			    margin: 0 auto;
+			}
+			
+			.loginPanel{
+				margin-top:100px;
+				padding:30px;
+				background-color: #ffffff;
+				box-shadow
+				-webkit-box-shadow: 6px 5px 6px 0px rgba(0,0,0,0.75);
+				-moz-box-shadow: 6px 5px 6px 0px rgba(0,0,0,0.75);
+				box-shadow: 6px 5px 6px 0px rgba(0,0,0,0.75);
+				border-radius:5px;
+			}
+			
+			.loginBanner{
+				margin-top: 70px;
+				text-align: center;
+				font-size: 40px;
+			}
+		</style>
+	<body>
+		<div class="row">
+			<div class="col-xs-12 col-sm-5 col-centered loginBanner">
+				Welcome! Please Login!	
+			</div>
+			<div class="col-xs-12 col-sm-5 col-centered loginPanel">
+				<form action="welcome.jsp" method="POST">
+					<div class="row">
+						<div class="col-xs-11 col-centered">
+							<div class="form-group">
+								<label for="username">Username</label>
+								<input type="text" name="username" class="form-control" id="username" placeholder="Username">
+							</div>
+							<div class="form-group">
+								<label for="password">Password</label>
+								<input type="password" name="password" class="form-control" id="password" placeholder="Password">
+							</div>
+							<button type="submit" class="btn btn-default pull-right">Submit</button>
+						 </div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ docs/Team19_DML.sql
@@ -0,0 +1,92 @@
+BEGIN;
+USE Team19_db;
+/* Users*/
+INSERT INTO User (Username,Password,Name,Type) VALUES ('user1','user','Bob','U');
+INSERT INTO User (Username,Password,Name,Type) VALUES ('user2','user','Coast Gaurd','U');
+INSERT INTO User (Username,Password,Name,Type) VALUES ('user3','user','Enviromental Research','U');
+INSERT INTO User (Username,Password,Name,Type) VALUES ('user4','user','Pounce City Hall','U');
+
+INSERT INTO User (Username,Password,Name,Type) VALUES ('user5','user','Jim','U');
+INSERT INTO User (Username,Password,Name,Type) VALUES ('user6','user','Acess Board','U');
+INSERT INTO User (Username,Password,Name,Type) VALUES ('user7','user','Enviromental Labs','U');
+INSERT INTO User (Username,Password,Name,Type) VALUES ('user8','user','Virginia Beach City Hall','U');
+
+/* Individual*/
+INSERT INTO Individual (Username,JobTitle,DateHired) VALUES ('user1',' Enviromental Engineer','2013-08-08');
+INSERT INTO Individual (Username,JobTitle,DateHired) VALUES ('user5','Resouce Manger','2014-07-08');
+
+/*Goverment Agency */
+INSERT INTO Government_Agency (Username,Jurisdiction) VALUES ('user2','Subject Matter Jurisdiction');
+INSERT INTO Government_Agency (Username,Jurisdiction) VALUES ('user6','Courts of General Jurisdiction');
+
+/*Company*/
+INSERT INTO Company (Username,HQLocation) VALUES ('user3','Atlanta');
+INSERT INTO Company (Username,HQLocation) VALUES ('user7','Virgina');
+
+/*Municipalities*/
+INSERT INTO Municipalities (Username,Population) VALUES ('user4','10000');
+INSERT INTO Municipalities (Username,Population) VALUES ('user8','1000000');
+
+/*ESF*/
+INSERT INTO ESF(Number,Description) VALUES (1, 'Transportation');
+INSERT INTO ESF(Number,Description) VALUES (2, 'Communications');
+INSERT INTO ESF(Number,Description) VALUES (3, 'Public Works and Engineering');
+INSERT INTO ESF(Number,Description) VALUES (4, 'Firefighting');
+INSERT INTO ESF(Number,Description) VALUES (5, 'Emergency Management');
+INSERT INTO ESF(Number,Description) VALUES (6, 'Mass Care, Emergency Assistance, Housing, and Human Services');
+INSERT INTO ESF(Number,Description) VALUES (7, 'Logistics Management and Resource Support');
+INSERT INTO ESF(Number,Description) VALUES (8, 'Public Health and Medical Services');
+INSERT INTO ESF(Number,Description) VALUES (9, 'Search and Rescue');
+INSERT INTO ESF(Number,Description) VALUES (10, 'Oil and Hazardous Materials Response');
+INSERT INTO ESF(Number,Description) VALUES (11, 'Agriculture and Natural Resources');
+INSERT INTO ESF(Number,Description) VALUES (12, 'External Affairs');
+INSERT INTO ESF(Number,Description) VALUES (13, 'Energy');
+INSERT INTO ESF(Number,Description) VALUES (14, 'Public Safety and Security');
+INSERT INTO ESF(Number,Description) VALUES (15, 'Long-Term Community Recovery');
+
+/*Cost Time Unit*/
+INSERT INTO Cost_Time_Unit(timeUnit) VALUES('Day');
+INSERT INTO Cost_Time_Unit(timeUnit) VALUES('Week');
+INSERT INTO Cost_Time_Unit(timeUnit) VALUES('Month');
+INSERT INTO Cost_Time_Unit(timeUnit) VALUES('Year');
+
+
+/*Resource*/
+INSERT INTO Resource (Username,name,NextAvailableDate,Status,Model,Latitude,Longitude,Amount,CostTimeUnit) VALUES ('user1','Resource1',CURDATE(),'Available', 'JKJDFD', '37.923122', '37.923122', '1123.20', 'Day');
+INSERT INTO Resource (Username,name,NextAvailableDate,Status,Model,Latitude,Longitude,Amount,CostTimeUnit) VALUES ('user1','Resource2',CURDATE(),'Available', 'DFDSFDS', '37.923122', '37.923122', '113.20', 'Week');
+INSERT INTO Resource (Username,name,NextAvailableDate,Status,Model,Latitude,Longitude,Amount,CostTimeUnit) VALUES ('user1','Resource3',CURDATE(),'Available', 'DFSDFSD', '37.923122', '37.923122', '1123.20', 'Month');
+INSERT INTO Resource (Username,name,NextAvailableDate,Status,Model,Latitude,Longitude,Amount,CostTimeUnit) VALUES ('user2','Resource3',CURDATE(),'Not Available', 'DSFSDF', '37.923122', '37.923122', '1123.20', 'Month');
+INSERT INTO Resource (Username,name,NextAvailableDate,Status,Model,Latitude,Longitude,Amount,CostTimeUnit) VALUES ('user2','Resource4',CURDATE(),'Not Available', 'DSFSDF', '37.923122', '37.923122', '1123.20', 'Week');
+INSERT INTO Resource (Username,name,NextAvailableDate,Status,Model,Latitude,Longitude,Amount,CostTimeUnit) VALUES ('user2','Resource4',CURDATE(),'Not Available', 'DSFSDF', '37.923122', '37.923122', '1123.20', 'Week');
+INSERT INTO Resource (Username,name,NextAvailableDate,Status,Model,Latitude,Longitude,Amount,CostTimeUnit) VALUES ('user3','Resource4',CURDATE(),'Not Available', 'DSFSDF', '37.923122', '37.923122', '1123.20', 'Week');
+INSERT INTO Resource (Username,name,NextAvailableDate,Status,Model,Latitude,Longitude,Amount,CostTimeUnit) VALUES ('user4','Resource4',CURDATE(),'Not Available', 'DSFSDF', '37.923122', '37.923122', '1123.20', 'Week');
+INSERT INTO Resource (Username,name,NextAvailableDate,Status,Model,Latitude,Longitude,Amount,CostTimeUnit, ID) VALUES ('user5','Resource4',CURDATE(),'Not Available', 'DSFSDF', '37.923122', '37.923122', '1123.20', 'Week', 100);
+INSERT INTO Resource (Username,name,NextAvailableDate,Status,Model,Latitude,Longitude,Amount,CostTimeUnit, ID) VALUES ('user5','Resource4',CURDATE(),'Not Available', 'DSFSDF', '37.923122', '37.923122', '1123.20', 'Week', 101);
+INSERT INTO Resource (Username,name,NextAvailableDate,Status,Model,Latitude,Longitude,Amount,CostTimeUnit, ID) VALUES ('user5','Resource4',CURDATE(),'Not Available', 'DSFSDF', '37.923122', '37.923122', '1123.20', 'Week', 102);
+
+/*Capabilities*/
+INSERT INTO Capabilities (ID,Capabilities ) VALUES (100, 'Patrolling');
+INSERT INTO Capabilities (ID,Capabilities ) VALUES (100, 'OnBoard Computer');
+INSERT INTO Capabilities (ID,Capabilities ) VALUES (100, 'GPS');
+
+
+/*Incident*/
+INSERT INTO Incident (Username,Date, Description, Latitude,Longitude) VALUES ('user1', CURDATE(), 'Test Incident 1', '37.12323','12.2123');
+INSERT INTO Incident (Username,Date, Description, Latitude,Longitude) VALUES ('user1', CURDATE(), 'Test Incident 2', '37.12323','12.2123');
+INSERT INTO Incident (Username,Date, Description, Latitude,Longitude) VALUES ('user1', CURDATE(), 'Test Incident 3', '37.12323','12.2123');
+INSERT INTO Incident (Username,Date, Description, Latitude,Longitude) VALUES ('user7', CURDATE(), 'Test Incident 1', '37.12323','12.2123');
+INSERT INTO Incident (Username,Date, Description, Latitude,Longitude) VALUES ('user8', CURDATE(), 'Test Incident 2', '37.12323','12.2123');
+INSERT INTO Incident (Username,Date, Description, Latitude,Longitude) VALUES ('user5', CURDATE(), 'Test Incident 3', '37.12323','12.2123');
+INSERT INTO Incident (ID, Username,Date, Description, Latitude,Longitude) VALUES ( 102,'user1', CURDATE(), 'Test Incident 3', '37.12323','12.2123');
+INSERT INTO Incident (ID, Username,Date, Description, Latitude,Longitude) VALUES (103,'user1', CURDATE(), 'Test Incident 3', '37.12323','12.2123');
+
+/*Requests*/
+INSERT INTO Requests(IncidentID, ResourceID, RequestDate, ReturnDate, ResourceOwner, Submitter) VALUES (102, 100,CURDATE(), DATE_ADD(CURDATE(),INTERVAL 6 MONTH),'user5', 'user1' );
+INSERT INTO Requests(IncidentID, ResourceID, RequestDate, ReturnDate, ResourceOwner, Submitter) VALUES (103, 101, CURDATE(), DATE_ADD(CURDATE(),INTERVAL 6 MONTH),'user5', 'user1' );
+
+/*Schedules_Repair*/
+INSERT INTO Schedules_Repair (Username,ResourceID, RepairStartDate, DaysInRepair) VALUES ('user1', 1, CURDATE(), '50');
+INSERT INTO Schedules_Repair (Username,ResourceID, RepairStartDate, DaysInRepair) VALUES ('user5', 102, CURDATE(), '50');
+
+
+COMMIT;
